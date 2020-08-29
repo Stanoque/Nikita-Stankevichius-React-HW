f5321a6a22451c6ebca1398dfca73e608af65d1a
Stanoque <stanoquedzhumpsh@gmail.com>
2020-08-29 15:11:01 +0300

    hw-1 is done.

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href= 'http://localhost:8888'>Navigate to spotify login page</a>
+    </div>
+  );
+}
+
+export default App;
